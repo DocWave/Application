4fc935f1755c10889b990bc713f8b21e4b9cf1cd
gcwelborn <gcruzwelborn@gmail.com>
2016-02-04 19:36:20 -0800

    react, gulp, photon,
    - react files for view folder
    - gulp file to compile react
    - photon kit css/fonts for UI styling (still needs to be ‘reactified’)
    —altered photonkit to add rounded search form, currently commented out
    after seeing photon comes with square search form. may use later
    though..
    - readme: took off the 3 waffle badges
    - more dependencies in package.json file for react, gulp.
    - added build folder (browserify bundle.js) to .gitignore

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 [![Stories in Ready](https://badge.waffle.io/lfox91/Doc-tor.png?label=ready&title=Ready)](https://waffle.io/lfox91/Doc-tor)
-[![Stories in Ready](https://badge.waffle.io/lfox91/Doc-tor.png?label=ready&title=Ready)](https://waffle.io/lfox91/Doc-tor)
-[![Stories in Ready](https://badge.waffle.io/lfox91/Doc-tor.png?label=ready&title=Ready)](https://waffle.io/lfox91/Doc-tor)
+
 # Doc-tor
 Free Javascript Documentation repository
--- main.js
+++ main.js
@@ -10,46 +10,91 @@ const menu = require('electron').Menu;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+function createWindow() {
+	// Create the browser window.
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+	let windowOptions = {
+		width: 800,
+		height: 600
+	}
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+	mainWindow = new BrowserWindow(windowOptions);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-  initMenu();
+	// and load the index.html of the app.
+	mainWindow.loadURL('file://' + __dirname + '/view/index.html');
+
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools();
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+	initMenu();
 }
 
 ///Menu
-function initMenu(){
+function initMenu() {
 	var Menu = require("menu");
 	var template = [{
-	    label: "Application",
-	    submenu: [
-	        { label: "Doc-tor", selector: "orderFrontStandardAboutPanel:" },
-	        { type: "separator" },
-	        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-	    ]}, {
-	    label: "Edit",
-	    submenu: [
-	        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-	        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-	        { type: "separator" },
-	        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-	        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-	        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-	        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-	    ]}
+			label: "Application",
+			submenu: [
+				{
+					label: "Doc-tor",
+					selector: "orderFrontStandardAboutPanel:"
+				},
+				{
+					type: "separator"
+				},
+				{
+					label: "Quit",
+					accelerator: "Command+Q",
+					click: function() {
+						app.quit();
+					}
+				}
+	    ]
+		}, {
+			label: "Edit",
+			submenu: [
+				{
+					label: "Undo",
+					accelerator: "CmdOrCtrl+Z",
+					selector: "undo:"
+				},
+				{
+					label: "Redo",
+					accelerator: "Shift+CmdOrCtrl+Z",
+					selector: "redo:"
+				},
+				{
+					type: "separator"
+				},
+				{
+					label: "Cut",
+					accelerator: "CmdOrCtrl+X",
+					selector: "cut:"
+				},
+				{
+					label: "Copy",
+					accelerator: "CmdOrCtrl+C",
+					selector: "copy:"
+				},
+				{
+					label: "Paste",
+					accelerator: "CmdOrCtrl+V",
+					selector: "paste:"
+				},
+				{
+					label: "Select All",
+					accelerator: "CmdOrCtrl+A",
+					selector: "selectAll:"
+				}
+	    ]
+		}
 	];
 	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
@@ -59,18 +104,18 @@ function initMenu(){
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function() {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('activate', function() {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
