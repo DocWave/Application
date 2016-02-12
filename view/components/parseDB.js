a1fa4c3753c277dc9db2129d64cae53afd934d5f
lfox91 <lsfox91@gmail.com>
2016-02-12 10:23:56 -0800

    grabbing new updates

--- view/components/parseDB.js
+++ view/components/parseDB.js
@@ -6,18 +6,18 @@ let filebuffer = fs.readFileSync('/Users/Fox/Documents/Programming/Doc-tor/docSt
 // Load the db
 let db = new SQL.Database(filebuffer);
 let nodeColArr = ['chapter', 'class', 'event', 'method', 'module', 'property'];
+let result = [];
 function parseSQL(colNameArr){
-  let result = [];
+
   function getObj(colName){
     let stmt = db.prepare(`SELECT TYPE, NAME, LINK FROM docsearch WHERE TYPE='${colName}'`);
-    stmt.getAsObject({':TYPE':{':NAME' : ':LINK'}});
+    stmt.getAsObject(':TYPE',':NAME',':LINK');
     while(stmt.step()) {
       result.push(stmt.getAsObject());
     }
-
-    console.log(result);
   }
   colNameArr.forEach(elem => getObj(elem));
+  return result;
 }
 
- parseSQL(nodeColArr);
+parseSQL(nodeColArr);
